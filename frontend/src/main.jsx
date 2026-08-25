c213323ec9ccbfc4b2056ce9d7028d478761f91d
Green Voice Zanzibar <greenvoicezanzibar@gmail.com>
2026-08-25 14:19:24 +0300

    Set favicon from backend profile photo on startup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { profileAPI } from './services/api'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function setFaviconFromProfile() {
+  try {
+    const res = await profileAPI.getProfile();
+    const profile = res?.data ?? res;
+    const url = profile?.profile_photo;
+    if (url) {
+      let link = document.querySelector("link[rel~='icon']");
+      if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.getElementsByTagName('head')[0].appendChild(link);
+      }
+      link.href = url;
+    }
+  } catch (e) {
+    // ignore - leave existing favicon
+    // console.warn('Could not set favicon from profile', e);
+  }
+}
+
+// Try to set favicon quickly before mounting React
+setFaviconFromProfile().finally(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+});
